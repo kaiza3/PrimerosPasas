50218ae92d40c98cf2ff38ddb5b1a0bd49c14d5c
kaiza3 <eduardmarculescu17@gmail.com>
2026-09-25 11:23:55 +0200

    Refuerzi

---
+++ src/refuerzi_1/re10.java
@@ -0,0 +1,7 @@
+package refuerzi_1;
+
+public class re10 {
+    public static void main(String[] args) {
+
+    }
+}
--- src/refuerzi_1/re9.java
+++ src/refuerzi_1/re9.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class re9 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Dime un numero: ");
+        int numero = sc.nextInt();
+        if (numero % 2 == 0 && numero % 3 !=0){
+            System.out.println("es posible de 2 pero no de 3");
+
+        }else {
+            System.out.println("no cumple la s condiciones");
+        }
 
     }
 }
